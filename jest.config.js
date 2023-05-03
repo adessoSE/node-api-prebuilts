589c5a5010ff744b8dae6d232afb99e5749f1c0c
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2023-05-03 17:19:00 +0200

    test: Fix ts-jest module handling

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,16 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: "ts-jest",
+  preset: "ts-jest/presets/default-esm",
   testEnvironment: "node",
+  moduleNameMapper: {
+    "^(\\.{1,2}/.*)\\.js$": "$1",
+  },
   transform: {
     "^.+\\.tsx?$": [
       "ts-jest",
       {
         tsconfig: "tsconfig.test.json",
+        useESM: true,
       },
     ],
   },
