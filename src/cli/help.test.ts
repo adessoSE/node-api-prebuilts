ccb44dc2cf335f9b9893b2cf93764ff25643df74
Henrik S. Gaßmann <henrik.gassmann@adesso.de>
2025-03-02 17:11:51 +0000

    test: switch from jest to vitest

--- src/cli/help.test.ts
+++ src/cli/help.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it, jest } from "@jest/globals";
+import { describe, expect, it, vi } from "vitest";
 
 import { helpCommand } from "./help";
 
@@ -6,7 +6,7 @@ describe("help-command", () => {
   // since entrypoint.cjs isn't compiled, we use this test case to assert that
   // help.ts is abiding its API contract
   it("should export a default function which prints something to the console", () => {
-    console.log = jest.fn();
+    console.log = vi.fn();
     helpCommand();
     expect(console.log).toHaveBeenCalled();
   });
